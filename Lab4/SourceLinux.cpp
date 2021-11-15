24128bcd18e7726963f64df9b92f57e08c3dbb52
Teodor Bogdan Vasilescu <vasilescu.teodor.c9j@student.ucv.ro>
2021-11-15 16:00:59 +0200

    add comments

--- Lab4/SourceLinux.cpp
+++ Lab4/SourceLinux.cpp
@@ -8,20 +8,24 @@ int main(){
     int id;
     for (int i = 0; i < 3; i++)
     {
+        //creating the child process
         id = fork();
+        //only the child process will enter this block
         if (id == 0)
         {
             sleep(i+1);
             std::cout << "Process " << i + 1 << " was executed." << std::endl;
             return 0;
+        //the child process ends execution here
         } else
+        //when the child process isn't created
         if (id < 0)
         {
             std::cout << "The fork has failed !!!" << std::endl;
             return 0;
         }
     }
-
+    //the parent process waits for his children to end execution
     for (int i = 0; i < 3; i++)
         wait(NULL);
     std::cout << "All processes have finished!!!";
